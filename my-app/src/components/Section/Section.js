11438b8ca3723be04c6488d64f7b2731f457cd95
TOR968 <tor968@yahoo.com>
2020-12-07 23:01:25 +0200

    Update Section.js

--- my-app/src/components/Section/Section.js
+++ my-app/src/components/Section/Section.js
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types';
 import styles from './Section.module.css';
 
-const Section = ({ title, children }) => {
+function Section({ title, children }) {
   return (
     <section className={styles.section}>
       <h2>{title}</h2>
       {children}
     </section>
   );
-};
+}
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
